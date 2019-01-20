725d2660b2dc958a64926e471268493dee50c7cf
abhaykv04 <abhaykv04@gmail.com>
2019-01-20 18:29:25 +0530

    Add more starter code (index.js)

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
