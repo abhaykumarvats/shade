382eb36ae5adde02f364f108a41d10a75efb9db3
abhaykv04 <abhaykv04@gmail.com>
2019-01-20 22:33:09 +0530

    Add post requests handler for /signup

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+// Require User schema
+const User = require('./schemas/User');
+
 // Require path for path-joining
 const path = require('path');
 
@@ -23,11 +26,34 @@ app.get('/', (req, res) => {
     res.redirect('/signup');
 });
 
-// GET requests handler for /signup
-app.get('/signup', (req, res) => {
-    // Send signup.html
-    res.sendFile(path.join(__dirname, 'public/signup.html'));
-});
+// GET and POST requests handler for /signup
+app.route('/signup')
+    .get((req, res) => {
+        // Send signup.html
+        res.sendFile(path.join(__dirname, 'public/signup.html'));
+    })
+    .post((req, res) => {
+        // Prepare user object
+        const user = {
+            username: req.body.username,
+            password: req.body.password,
+            consent: req.body.consent ? true : false
+        };
+
+        console.log('Consent:', req.body.consent);
+
+        // Create and save user object into database
+        User.create(user, (err, userRecord) => {
+            // Log error, if any
+            if (err) console.error(err);
+
+            // Log returned user record
+            console.log(userRecord);
+
+            // Redirect to /login page
+            res.redirect('/login');
+        });
+    });
 
 // GET requests handler for /login
 app.get('/login', (req, res) => {
