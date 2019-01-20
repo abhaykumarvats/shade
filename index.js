712aff6f201c45733d4cfce3d57db83babdaecbb
abhaykv04 <abhaykv04@gmail.com>
2019-01-20 20:39:40 +0530

    Write comments in index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+// Require and instantiate express
 const express = require('express');
 const app = express();
 
+// Require path for path-joining
+const path = require('path');
+
+// Retrieve port number from environment, or use 3000
 const port = process.env.PORT || 3000;
 
+// Set view engine as pug
 app.set('view engine', 'pug');
 
-app.use(express.static(__dirname + '/public'));
+// Serve static files from /public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse body of incoming requests
 app.use(express.urlencoded({ extended: false }));
 
+// GET requests handler for / route
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
+// Start server and listen for requests
 app.listen(port, () => console.log('Listening on port', port));
