8f840c7058d71fa3bff5a8ec5a9e39eccaf2ae63
abhaykv04 <abhaykv04@gmail.com>
2019-01-22 20:30:52 +0530

    Change indentation from 4 to 2

--- index.js
+++ index.js
@@ -22,67 +22,69 @@ app.use(express.urlencoded({ extended: false }));
 
 // GET requests handler for / route
 app.get('/', (req, res) => {
-    // Redirect all the to /signup
-    res.redirect('/signup');
+  // Redirect all the to /signup
+  res.redirect('/signup');
 });
 
 // GET and POST requests handler for /signup
-app.route('/signup')
-    .get((req, res) => {
-        // Send signup.html
-        res.sendFile(path.join(__dirname, 'public/signup.html'));
-    })
-    .post((req, res) => {
-        // Prepare user object
-        const user = {
-            username: req.body.username,
-            password: req.body.password,
-            consent: req.body.consent ? true : false
-        };
+app
+  .route('/signup')
+  .get((req, res) => {
+    // Send signup.html
+    res.sendFile(path.join(__dirname, 'public/signup.html'));
+  })
+  .post((req, res) => {
+    // Prepare user object
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      consent: req.body.consent ? true : false
+    };
 
-        // Create and save user object into database
-        User.create(user, (err, userRecord) => {
-            // Log error, if any
-            if (err) {
-                console.error(err);
-                res.redirect('/signup');
-                return;
-            }
+    // Create and save user object into database
+    User.create(user, (err, userRecord) => {
+      // Log error, if any
+      if (err) {
+        console.error(err);
+        res.redirect('/signup');
+        return;
+      }
 
-            // Log returned user record
-            console.log(userRecord);
+      // Log returned user record
+      console.log(userRecord);
 
-            // Redirect to /login page
-            res.redirect('/login');
-        });
+      // Redirect to /login page
+      res.redirect('/login');
     });
+  });
 
 // GET and POST requests handler for /login
-app.route('/login')
-    .get((req, res) => {
-        // Send login.html
-        res.sendFile(path.join(__dirname, 'public/login.html'));
-    })
-    .post((req, res) => {
-        // Prepare user object
-        const user = {
-            username: req.body.username,
-            password: req.body.password
-        };
+app
+  .route('/login')
+  .get((req, res) => {
+    // Send login.html
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+  })
+  .post((req, res) => {
+    // Prepare user object
+    const user = {
+      username: req.body.username,
+      password: req.body.password
+    };
 
-        // Try to find user
-        User.findOne(user, '-_id username consent', (err, userRecord) => {
-            // Log error, if any
-            if (err) {
-                console.error(err);
-                res.redirect('/login');
-                return;
-            }
+    // Try to find user
+    User.findOne(user, '-_id username consent', (err, userRecord) => {
+      // Log error, if any
+      if (err) {
+        console.error(err);
+        res.redirect('/login');
+        return;
+      }
 
-            // Send response as json, if found
+      // Send response as json, if found
       userRecord ? res.json(userRecord) : res.send('<h1>No such user.</h1>');
-        });
     });
+  });
 
 // Start server and listen for requests
 app.listen(PORT, () => console.log('Listening on port', PORT));
--- schemas/User.js
+++ schemas/User.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 
 // Connect mongoose to database
 mongoose.connect(
-    process.env.DATABASE_URI,
-    { useNewUrlParser: true }
+  process.env.DATABASE_URI,
+  { useNewUrlParser: true }
 );
 
 // Create userSchema from mongoose.Schema
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    consent: {
-        type: Boolean,
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  consent: {
+    type: Boolean,
+    required: true
+  }
 });
 
 // Export userSchema as User model
