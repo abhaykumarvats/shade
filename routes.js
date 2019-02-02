9663f851594fef05a5f1cfc64dfcb14affc9741f
abhaykv04 <abhaykv04@gmail.com>
2019-02-02 14:33:28 +0530

    Remove ensureAuthentication(), and check auth in route handlers

--- routes.js
+++ routes.js
@@ -12,18 +12,9 @@ const User = require('./schemas/User');
 
 // Export routes module
 module.exports = (app) => {
-  // Check if user is logged in
-  function ensureAuthentication(req, res, next) {
-    // User is logged in
-    if (req.isAuthenticated()) return next();
-
-    // User is not logged in
-    res.redirect('/login');
-  }
-
   // GET requests handler, for /
-  app.route('/').get(ensureAuthentication, (req, res) => {
-    // Redirect to home page
+  app.route('/').get((req, res) => {
+    // Redirect to /home
     res.redirect('/home');
   });
 
@@ -31,23 +22,35 @@ module.exports = (app) => {
    * TODO: Implement below two handlers
    */
   // GET requests handler, for /home
-  app.route('/home').get(ensureAuthentication, (req, res) => {
-    // TODO: Render home page
-    res.send('Home page. <a href="/logout">Log Out.</a>');
+  app.route('/home').get((req, res) => {
+    // Check if user is logged in
+    if (req.isAuthenticated())
+      // TODO: User is logged in, render home page
+      res.send('Home page. <a href="/logout">Log Out.</a><br>' + req.user);
+    // User is not logged in, redirect to /login
+    else res.redirect('/login');
   });
 
   // GET requests handler, for /profile
-  app.route('/profile').get(ensureAuthentication, (req, res) => {
-    // TODO: Render profile page
-    res.send('Profile page. <a href="/logout">Log Out.</a>');
+  app.route('/profile').get((req, res) => {
+    // Check if user is logged in
+    if (req.isAuthenticated())
+      // TODO: User is logged in, render profile page
+      res.send('Profile page. <a href="/logout">Log Out.</a><br>' + req.user);
+    // User is not logged in, redirect to /login
+    else res.redirect('/login');
   });
 
   // GET and POST requests handler, for /login
   app
     .route('/login')
     .get((req, res) => {
-      // Render login page
-      res.render('form', { type: 'login' });
+      // Check if user is logged in
+      if (req.isAuthenticated())
+        // User is logged in, redirect to /home
+        res.redirect('/home');
+      // User is not logged in, render login form
+      else res.render('form', { type: 'login' });
     })
     .post(
       // Authenticate user with local strategy
@@ -61,8 +64,12 @@ module.exports = (app) => {
   app
     .route('/register')
     .get((req, res) => {
-      // Render register page
-      res.render('form', { type: 'register' });
+      // Check if user is logged in
+      if (req.isAuthenticated())
+        // User is logged in, redirect to /home
+        res.redirect('/home');
+      // User is not logged in, render register form
+      else res.render('form', { type: 'register' });
     })
     .post(
       (req, res, next) => {
@@ -70,7 +77,7 @@ module.exports = (app) => {
         User.findOne({ username: req.body.username }, (err, user) => {
           // If error, pass it to next middleware
           if (err) next(err);
-          // Else, if user is already registered
+          // If user is already registered
           else if (user) res.redirect('/login');
           // Else, proceed with registration
           else {
