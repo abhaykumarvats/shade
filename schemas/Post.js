0db2e96a85efba9127e438bfd112d18c15ed1e6c
abhaykv04 <abhaykv04@gmail.com>
2019-02-06 23:19:09 +0530

    Add postSchema

---
+++ schemas/Post.js
@@ -0,0 +1,32 @@
+// Require mongoose
+const mongoose = require('mongoose');
+
+// Connect mongoose to database
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+// Create postSchema from mongoose.Schema
+const postSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  postedOn: {
+    type: Date,
+    default: Date.now
+  },
+  meantFor: {
+    type: [String],
+    required: true
+  },
+  seenby: [String]
+});
+
+// Convert postSchema into Post model, and export
+module.exports = mongoose.model('Post', postSchema);
