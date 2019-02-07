0cca65ef27db99713f0047c9c34cdcaae3a626a5
abhaykv04 <abhaykv04@gmail.com>
2019-02-08 01:18:57 +0530

    Show 'Required' small text when username/password field is empty

--- public/js/login.js
+++ public/js/login.js
@@ -18,14 +18,24 @@ $(document).ready(() => {
     password = $('#password').val();
     passwordSmallText = $('#password-small-text');
 
+    // If username field is empty
+    if (!username) {
+      usernameSmallText.addClass('text-danger').text('Required');
+      return false;
+    }
     // If username is short
-    if (username.length < 4) {
+    else if (username.length < 4) {
       usernameSmallText.addClass('text-danger').text('Invalid');
       return false;
     }
 
+    // If password field is empty
+    if (!password) {
+      passwordSmallText.addClass('text-danger').text('Required');
+      return false;
+    }
     // If password is short
-    if (password.length < 6) {
+    else if (password.length < 6) {
       passwordSmallText.addClass('text-danger').text('Invalid');
       return false;
     }
