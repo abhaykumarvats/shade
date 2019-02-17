0516f451173f8abe49b708dba96786cdddbf630b
abhaykv04 <abhaykv04@gmail.com>
2019-02-17 17:56:17 +0530

    Add client-side logic to get and show user's public posts

--- public/js/profile/public.js
+++ public/js/profile/public.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
+  const username = $('#username').text();
+
   const activityTab = $('#activity-tab');
   const aboutTab = $('#about-tab');
 
   const activityContainer = $('#activity-container');
   const aboutContainer = $('#about-container');
+
+  const activityList = $('#activity-list');
+  const activitySmallText = $('#activity-small-text');
+
+  let skip = 0;
+  const limit = 20;
 
   // Trigger when activityTab is clicked
   activityTab.click(() => {
@@ -29,5 +37,50 @@ $(document).ready(() => {
     aboutContainer.css('display', 'block');
 
     return false;
+  });
+
+  // Show Loading... small text
+  activitySmallText.html('<small>Loading...</small>');
+
+  // Get user's public posts
+  $.getJSON(username + '/posts/public/' + skip + '/' + limit, (json) => {
+    const jsonLength = json.length;
+
+    // For each public post
+    for (let i = 0; i < json.length; i++) {
+      // Append a list group item
+      activityList.append(
+        '<li class="list-group-item">' +
+          '<h6 class="mb-0">' +
+          json[i].username +
+          '<small class="text-muted float-right">' +
+          new Date(json[i].date)
+            .toString()
+            .split(' ')
+            .slice(1, 4)
+            .join(' ') +
+          '</small>' +
+          '</h6>' +
+          '<p class="mb-0">' +
+          json[i].content +
+          '</p>' +
+          '</li>'
+      );
+    }
+
+    // If no posts are found
+    if (!jsonLength) {
+      activitySmallText.html('<small>Nothing to see here!</small>');
+    }
+    // If less than 20 posts are found
+    else if (jsonLength <= 20) {
+      activitySmallText.html("<small>That's it, folks!</small>");
+    } else {
+      // Show 'More' link
+      activitySmallText
+        .removeClass('text-center text-muted')
+        .addClass('text-right')
+        .html('<a href="#">More</a>');
+    }
   });
 });
