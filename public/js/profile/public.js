8afcda756fe6e683784e633b0fc2e490e6c9af64
abhaykv04 <abhaykv04@gmail.com>
2019-02-18 15:42:37 +0530

    Reorder, refactor profile/public.js

--- public/js/profile/public.js
+++ public/js/profile/public.js
@@ -11,7 +11,58 @@ $(document).ready(() => {
   const activitySmallText = $('#activity-small-text');
 
   let skip = 0;
-  const limit = 20;
+  const limit = 10;
+
+  // Show Loading... small text
+  activitySmallText.addClass('text-muted').html('<small>Loading...</small>');
+
+  // Get user's public posts
+  $.getJSON(username + '/posts/public/' + skip + '/' + limit, (json) => {
+    const jsonLength = json.length;
+
+    // No initial posts found
+    if (!jsonLength) {
+      activitySmallText
+        .addClass('text-muted')
+        .html('<small>Nothing to see here!</small>');
+    } else {
+      // For each public post
+      for (let i = 0; i < jsonLength; i++) {
+        // Append a list group item
+        activityList.append(
+          '<li class="list-group-item">' +
+            '<h6 class="mb-0">' +
+            json[i].username +
+            '<small class="text-muted float-right">' +
+            new Date(json[i].date)
+              .toString()
+              .split(' ')
+              .slice(1, 4)
+              .join(' ') +
+            '</small>' +
+            '</h6>' +
+            '<p class="mb-0">' +
+            json[i].content +
+            '</p>' +
+            '</li>'
+        );
+      }
+
+      // If less than 10 posts were found
+      if (jsonLength < 10) {
+        activitySmallText
+          .addClass('text-muted')
+          .html("<small>That's it, folks!</small>");
+      } else {
+        // Show 'More' link
+        activitySmallText
+          .removeClass('text-muted')
+          .html('<a href="#" id="more"><small>More</small></a>');
+
+        // TODO
+      }
+    }
+  });
 
   // Trigger when activityTab is clicked
   activityTab.click(() => {
@@ -37,50 +88,5 @@ $(document).ready(() => {
     aboutContainer.css('display', 'block');
 
     return false;
-  });
-
-  // Show Loading... small text
-  activitySmallText.html('<small>Loading...</small>');
-
-  // Get user's public posts
-  $.getJSON(username + '/posts/public/' + skip + '/' + limit, (json) => {
-    const jsonLength = json.length;
-
-    // For each public post
-    for (let i = 0; i < json.length; i++) {
-      // Append a list group item
-      activityList.append(
-        '<li class="list-group-item">' +
-          '<h6 class="mb-0">' +
-          json[i].username +
-          '<small class="text-muted float-right">' +
-          new Date(json[i].date)
-            .toString()
-            .split(' ')
-            .slice(1, 4)
-            .join(' ') +
-          '</small>' +
-          '</h6>' +
-          '<p class="mb-0">' +
-          json[i].content +
-          '</p>' +
-          '</li>'
-      );
-    }
-
-    // If no posts are found
-    if (!jsonLength) {
-      activitySmallText.html('<small>Nothing to see here!</small>');
-    }
-    // If less than 20 posts are found
-    else if (jsonLength <= 20) {
-      activitySmallText.html("<small>That's it, folks!</small>");
-    } else {
-      // Show 'More' link
-      activitySmallText
-        .removeClass('text-center text-muted')
-        .addClass('text-right')
-        .html('<a href="#">More</a>');
-    }
   });
 });
