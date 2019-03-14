592671936d420745499d743df0f52f91e0ed64cf
abhaykv04 <abhaykv04@gmail.com>
2019-03-14 18:12:12 +0530

    Add client-side logic to get and show connection-only posts

--- public/js/profile/connection.js
+++ public/js/profile/connection.js
@@ -7,7 +7,13 @@ $(document).ready(() => {
   const activityContainer = $('#activity-container');
   const aboutContainer = $('#about-container');
 
+  const activityList = $('#activity-list');
+  const activityEndText = $('#activity-end-text');
+
   const aboutList = $('#about-list');
+
+  let skip = 0;
+  const limit = 10;
 
   // Trigger when activity tab is clicked
   activityTab.click(() => {
@@ -16,6 +22,88 @@ $(document).ready(() => {
 
     aboutContainer.css('display', 'none');
     activityContainer.css('display', 'block');
+
+    return false;
+  });
+
+  // Function to get connection-only and public posts
+  function getConnectionPosts(skip, limit) {
+    // Get user's posts
+    $.getJSON(username + '/posts/connection/' + skip + '/' + limit, (json) => {
+      const jsonLength = json.length;
+
+      // No posts found
+      if (!jsonLength) {
+        // If no initial posts found
+        if (!skip) {
+          activityEndText
+            .removeAttr('href')
+            .addClass('text-muted')
+            .text('Nothing to see here!')
+            .off('click');
+        }
+        // If no further posts found
+        else {
+          activityEndText
+            .removeAttr('href')
+            .addClass('text-muted')
+            .text("That's all, folks!")
+            .off('click');
+        }
+      } else {
+        // For each post
+        for (let i = 0; i < jsonLength; i++) {
+          // Append a list group item
+          activityList.append(
+            '<li class="list-group-item">' +
+              '<h6 class="mb-0">' +
+              json[i].username +
+              '<small class="text-muted float-right">' +
+              new Date(json[i].date)
+                .toString()
+                .split(' ')
+                .slice(0, 4)
+                .join(' ') +
+              '</small>' +
+              '</h6>' +
+              '<p class="mb-0">' +
+              json[i].content +
+              '</p>' +
+              '</li>'
+          );
+        }
+
+        // If less than 10 posts were found
+        if (jsonLength < 10) {
+          activityEndText
+            .removeAttr('href')
+            .addClass('text-muted')
+            .text("That's all, folks!")
+            .off('click');
+        } else {
+          // Show 'More' link
+          activityEndText
+            .attr('href', '#')
+            .removeClass('text-muted')
+            .text('More');
+        }
+      }
+    });
+  }
+
+  // Trigger when activity-end-text is clicked
+  activityEndText.click(() => {
+    // Show Loading... text
+    activityEndText
+      .removeAttr('href')
+      .addClass('text-muted')
+      .text('Loading...');
+
+    // Get connection-only and public posts
+    getConnectionPosts(skip, limit);
+
+    // Increment 'skip' by 10
+    skip += 10;
 
     return false;
   });
