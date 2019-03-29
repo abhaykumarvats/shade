b5d877256508eca607ad249d7bb8a6e2125558a8
abhaykv04 <abhaykv04@gmail.com>
2019-03-29 20:54:37 +0530

    Add client-side logic to get and show home posts in home view

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+  const username = $('#username').text();
+
   const activityTab = $('#activity-tab');
   const searchTab = $('#search-tab');
   const notifsTab = $('#notifs-tab');
@@ -7,11 +9,17 @@ $(document).ready(() => {
   const searchContainer = $('#search-container');
   const notifsContainer = $('#notifs-container');
 
+  const activityList = $('#activity-list');
+  const activityEndText = $('#activity-end-text');
+
   const searchInputField = $('#search-input');
   const searchButton = $('#search-button');
   const searchResult = $('#search-result');
 
   const redColor = '#dc3545';
+
+  let skip = 0;
+  const limit = 10;
 
   // Trigger when activity-tab is clicked
   activityTab.click(() => {
@@ -24,6 +32,88 @@ $(document).ready(() => {
     searchContainer.css('display', 'none');
     notifsContainer.css('display', 'none');
     activityContainer.css('display', 'block');
+
+    return false;
+  });
+
+  // Function to get home posts
+  function getHomePosts(skip, limit) {
+    // Get posts
+    $.getJSON(username + '/posts/home/' + skip + '/' + limit, (json) => {
+      const jsonLength = json.length;
+
+      // No posts found
+      if (!jsonLength) {
+        // If no initial posts found
+        if (!skip) {
+          activityEndText
+            .removeAttr('href')
+            .addClass('text-muted')
+            .text('Nothing to see here!')
+            .off('click');
+        }
+        // If no further posts found
+        else {
+          activityEndText
+            .removeAttr('href')
+            .addClass('text-muted')
+            .text("That's all, folks!")
+            .off('click');
+        }
+      } else {
+        // For each post
+        for (let i = 0; i < jsonLength; i++) {
+          // Append a list group item
+          activityList.append(
+            '<li class="list-group-item">' +
+              '<h6 class="mb-0">' +
+              json[i].username +
+              '<small class="text-muted float-right">' +
+              new Date(json[i].date)
+                .toString()
+                .split(' ')
+                .slice(0, 4)
+                .join(' ') +
+              '</small>' +
+              '</h6>' +
+              '<p class="mb-0">' +
+              json[i].content +
+              '</p>' +
+              '</li>'
+          );
+        }
+
+        // If less than 10 posts were found
+        if (jsonLength < 10) {
+          activityEndText
+            .removeAttr('href')
+            .addClass('text-muted')
+            .text("That's all, folks!")
+            .off('click');
+        } else {
+          // Show 'More' link
+          activityEndText
+            .attr('href', '#')
+            .removeClass('text-muted')
+            .text('More');
+        }
+      }
+    });
+  }
+
+  // Trigger when activity-end-text is clicked
+  activityEndText.click(() => {
+    // Show Loading... text
+    activityEndText
+      .removeAttr('href')
+      .addClass('text-muted')
+      .text('Loading...');
+
+    // Get home posts
+    getHomePosts(skip, limit);
+
+    // Increment 'skip' by 10
+    skip += 10;
 
     return false;
   });
